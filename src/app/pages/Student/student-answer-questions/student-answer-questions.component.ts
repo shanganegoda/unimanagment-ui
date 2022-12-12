2b255c8a4b86945224cfd903fdfb45e7348b049c
ishari-anjana <piumiganegoda@gmail.com>
2022-12-12 23:11:42 +0530

    save student answers

--- src/app/pages/Student/student-answer-questions/student-answer-questions.component.ts
+++ src/app/pages/Student/student-answer-questions/student-answer-questions.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
+import { StudentQuestion } from 'src/app/Models/student-question.model';
 
 @Component({
   selector: 'app-student-answer-questions',
@@ -8,7 +9,7 @@ import { ApiService } from 'src/app/api.service';
   styleUrls: ['./student-answer-questions.component.scss']
 })
 export class StudentAnswerQuestionsComponent implements OnInit {
-
+  studentQuestion:Array<StudentQuestion>=new Array<StudentQuestion>();
   quizId : any
   questions : any = {}
   constructor(private api : ApiService , private route:ActivatedRoute) { }
@@ -19,5 +20,23 @@ export class StudentAnswerQuestionsComponent implements OnInit {
       this.questions = res
     })
   }
+  setradio(e: string,a:number): void   
+  {  
+    //if(this.studentQuestion.forEach){
+//handle if already record exists
+    //}
+    let studentQuestion=new StudentQuestion();
+    studentQuestion.QuizId=this.quizId;
+    studentQuestion.AnswerId=a;
+    studentQuestion.QuestionId=parseInt(e);
+    studentQuestion.StudentId=1;  //ToDO:Get student Id currently all will save as 1
 
+  this.studentQuestion.push(studentQuestion)
+  } 
+  saveAnswers(){
+    this.api.postStudentQuestions(this.studentQuestion)
+  }
+  post(question){
+    question.quizId = this.quizId
+  }
 }
