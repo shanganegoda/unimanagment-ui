45cca374bebd399fe346279b16033ca12a366700
ishari-anjana <piumiganegoda@gmail.com>
2022-12-12 23:12:08 +0530

    student answer model

---
+++ src/app/Models/student-question.model.ts
@@ -0,0 +1,8 @@
+export class StudentQuestion {
+      Id :number=0;
+      StudentId :number=0;
+      QuizId :number=0;
+      QuestionId :number=0;
+      AnswerId :number=0;
+     CorrectAnswer:boolean=false;
+}
